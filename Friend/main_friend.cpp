4cc9efd3591275fdf714a4740b51867ed0d49f5d
admin <1397817633@qq.com>
2022-01-17 18:31:41 +0800

    feat(Friend): learn friend features

---
+++ Friend/main_friend.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+class A {
+public:
+    A(int a);
+    friend class B;
+    friend int friend_func(A &a);
+private:
+    int a_;
+};
+
+int friend_func(A &a) {
+    return a.a_;
+}
+
+A::A(int a) :
+    a_(a) {
+
+}
+
+class B
+{
+public:
+    B();
+    int get(A &a);
+
+};
+
+B::B()
+{
+}
+
+int B::get(A &a) {
+    return a.a_;
+}
+
+int main() {
+    A a(3);
+    B b;
+    std::cout << b.get(a) << std::endl;
+    std::cout << friend_func(a) << std::endl;
+}
