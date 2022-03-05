5f1fde61239a5996109d148244a89c27d1d20485
admin <1397817633@qq.com>
2022-03-05 15:36:22 +0800

    feat(map): judge std::map key is valid

--- Map/map_main.cpp
+++ Map/map_main.cpp
@@ -3,9 +3,16 @@
 
 int main() {
     std::map<int, float> test_map;
-    test_map.insert(std::make_pair(0, 2.1f));
+    test_map.insert(std::make_pair(2, 2.1f));
     test_map.insert(std::make_pair(1, 1.1f));
-    std::cout << test_map[0] << std::endl;
+    if (test_map.find(0) == test_map.end())
+        std::cout << "error" << std::endl;
+    else
+        std::cout << test_map[0] << std::endl;
+    if (test_map.count(1) == 0)
+        std::cout << "error" << std::endl;
+    else
+        std::cout << test_map[1] << std::endl;
     for (const auto &it : test_map) {
         std::cout << it.first << std::endl;
         std::cout << it.second << std::endl;
